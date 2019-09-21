94d87c5c148d49c8c0e2966adec28d4917409170
LoveEnergy <295351844@qq.com>
2019-09-21 14:16:02 +0800

    setUpProject

---
+++ woleyi-SwiftVersion/woleyi-SwiftVersion/Home/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  woleyi-SwiftVersion
+//
+//  Created by ENERGY on 2019/9/21.
+//  Copyright © 2019 ENERGY. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ woleyi-SwiftVersion/woleyi-SwiftVersion/SubstringFiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  woleyi-SwiftVersion
+//
+//  Created by ENERGY on 2019/9/21.
+//  Copyright © 2019 ENERGY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
